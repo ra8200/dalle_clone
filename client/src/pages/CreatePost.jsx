a5725c1f39ce289b9c158901f4d8358fb9ea17b7
ra8200 <razi.alawamleh@gmail.com>
2023-04-05 00:35:27 -0400

    forth commit updated routes for deployment

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://dall-e-clone-4fga.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -46,7 +46,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/posts', {
+        const response = await fetch('https://dall-e-clone-4fga.onrender.com/api/v1/posts', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/posts', {
+        const response = await fetch('https://dall-e-clone-4fga.onrender.com/api/v1/posts', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
